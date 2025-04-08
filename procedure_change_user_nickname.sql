fc3a4f1b2046c582433b60cfca93609b94d0ef1a
blackBuzo2601 <elianpro58@gmail.com>
2025-04-07 18:28:54 -0700

    Creado procedure para cambiar el nickname del usuario

---
+++ procedure_change_user_nickname.sql
@@ -0,0 +1,21 @@
+/*
+CHANGE_USER_NICKNAME
+Este procedure recibe el nickname con el que el usuario quiere reemplazar su nickname en la 
+información de su cuenta.
+El parametro p_user_email corresponde al campo que usa el backend para buscar el usuario
+en la db y reemplazar su información.
+*/
+USE lumitechDB;
+DELIMITER $$
+
+CREATE PROCEDURE change_user_nickname(
+    IN p_user_email VARCHAR(100),
+    IN p_user_nickname VARCHAR(15)
+)
+BEGIN
+    UPDATE user
+    SET user_nickname = p_user_nickname
+    WHERE user_email = p_user_email;
+END $$
+
+DELIMITER ;
