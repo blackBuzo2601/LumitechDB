6887f1ffb21726ff9a9319aa144cce5990d56924
blackBuzo2601 <elianpro58@gmail.com>
2025-05-02 18:33:56 -0700

    Esquema base de datos y procedures, actualizados al backendm mas reciente 2/5/2025, clase backend subida al servidor

--- register_rosette.sql
+++ register_rosette.sql
@@ -4,6 +4,7 @@ DELIMITER $$
 
 CREATE PROCEDURE register_rosette(
     IN p_rosette_mac VARCHAR(17),
+    IN p_rosette_ip VARCHAR(45),
     IN p_wifi_ssid VARCHAR(50),
     IN p_wifi_password VARCHAR(60),
     IN p_owner_uuid CHAR(36),
@@ -11,6 +12,7 @@ CREATE PROCEDURE register_rosette(
 )
 BEGIN
     DECLARE rosette_exists INT;
+    DECLARE p_rosette_ubication VARCHAR(30);
 
     -- Verificar si la roseta ya está registrada
     SELECT COUNT(*) INTO rosette_exists
@@ -20,19 +22,22 @@ BEGIN
     IF rosette_exists > 0 THEN
         SET p_message = 'Esta roseta ya está registrada';
     ELSE
+		SET p_rosette_ubication = 'Ubicacion Pendiente';
         -- Insertar en tabla rosette con nuevos campos
         INSERT INTO rosette (
             rosette_ubication,
             rosette_register_date,
             rosette_mac,
+            rosette_ip,
             wifi_ssid,
             wifi_password,
             owner_uuid
         )
         VALUES (
-            NULL,  -- Si la ubicación no se proporciona, se puede dejar como NULL
+            p_rosette_ubication, 
             NOW(),
             p_rosette_mac,
+            p_rosette_ip,
             p_wifi_ssid,
             p_wifi_password,
             p_owner_uuid
